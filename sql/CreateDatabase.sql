399ad1d0ca6cd042e5b3797f25606e4405498222
phansonloc1999 <phansonloc1999@gmail.com>
2022-04-13 15:27:15 +0700

    Use seperate sql files

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -1,18 +1,11 @@
 -- NOTE: Run this script with root/system admin or database administrator account
-
--- DROP DATABASE DiemDanh;
--- DROP USER 'sinhvien'@'%';
--- DROP USER 'giaovu'@'%';
-
 CREATE DATABASE DiemDanh;
--- CREATE USER 'sinhvien'@'%' IDENTIFIED BY 'sinhvien';
--- GRANT ALL ON DiemDanh.* TO 'sinhvien'@'%';
-
--- DROP DATABASE DiemDanh;
+CREATE USER 'sinhvien'@'%' IDENTIFIED BY 'sinhvien';
+GRANT ALL ON DiemDanh.* TO 'sinhvien'@'%';
 
 USE DiemDanh;
 
--- DROP TABLE mon_hoc IF EXISTS;
+-- DROP TABLE mon_hoc;
 CREATE TABLE mon_hoc (
     ma_mh CHAR(15) NOT NULL PRIMARY KEY,
     ten_mh VARCHAR(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
@@ -61,34 +54,3 @@ CREATE TABLE diem_danh_sv (
     FOREIGN KEY (ma_mh) REFERENCES mon_hoc(ma_mh),
     FOREIGN KEY (mssv) REFERENCES sinh_vien(mssv)
 );
-
--- DELETE FROM mon_hoc;
-INSERT INTO mon_hoc VALUES ('CSC001', 'Lập trình cơ bản');
-INSERT INTO mon_hoc VALUES ('CSC002', 'Lập trình hướng đối tượng');
-
-INSERT INTO sinh_vien VALUES (1712571, 'Phan Sơn Lộc', '1712571', '1712571');
-INSERT INTO sinh_vien VALUES (1712572, 'Phan Sơn Phúc', '1712572', '1712572');
-
-INSERT INTO thoi_khoa_bieu VALUES (1712571, CURRENT_DATE(), '2022-4-14', 2, CURRENT_TIME(), CURRENT_TIME(), 'E101', 'CSC002');
-INSERT INTO thoi_khoa_bieu VALUES (1712572, CURRENT_DATE(), '2022-4-14', 2, CURRENT_TIME(), CURRENT_TIME(), 'E101', 'CSC002');
-
-INSERT INTO danh_sach_sv_mh VALUES ('CSC002', 1712571);
-INSERT INTO danh_sach_sv_mh VALUES ('CSC002', 1712572);
-
-INSERT INTO diem_danh_sv (ma_mh, mssv, tuan, co_mat )
-VALUES 
-    ('CSC002', 1712571, 1, 0),
-    ('CSC002', 1712571, 2, 0),
-    ('CSC002', 1712571, 3, 0),
-    ('CSC002', 1712571, 4, 0),
-    ('CSC002', 1712571, 5, 0),
-    ('CSC002', 1712571, 6, 0),
-    ('CSC002', 1712571, 7, 0),
-    ('CSC002', 1712571, 8, 0),
-    ('CSC002', 1712571, 9, 0),
-    ('CSC002', 1712571, 10, 0),
-    ('CSC002', 1712571, 11, 0),
-    ('CSC002', 1712571, 12, 0),
-    ('CSC002', 1712571, 13, 0),
-    ('CSC002', 1712571, 14, 0),
-    ('CSC002', 1712571, 15, 0);
---
+++ sql/DropDB&Users.sql
@@ -0,0 +1,5 @@
+-- NOTE: Run this script with root/system admin or database administrator account
+
+DROP DATABASE DiemDanh;
+DROP USER 'sinhvien'@'%';
+DROP USER 'giaovu'@'%';
