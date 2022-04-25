118d3bc96251aed486e29bc25c34e939a5922f9b
phansonloc1999 <phansonloc1999@gmail.com>
2022-04-25 11:11:00 +0700

    Handle login for giaovu and sinhvien in App class

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,10 @@
 package com.example;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+
 import Utils.*;
 
 /**
@@ -7,13 +12,94 @@ import Utils.*;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        // GiaoVu giaoVu = new GiaoVu();
-        // giaoVu.setConnection(DatabaseUtils.getDBConnection("giaovu", "giaovu"));
-        // giaoVu.showMainMenu();
+    public final static String SV_DB_USERNAME = "sinhvien", SV_DB_PASSWORD = "sinhvien";
+    public final static String GV_DB_USERNAME = "giaovu", GV_DB_PASSWORD = "giaovu";
 
-        SinhVien sv = new SinhVien();
-        sv.setConnection(DatabaseUtils.getDBConnection("sinhvien", "sinhvien"));
-        sv.showLogin();
+    public static void main(String[] args) {
+        final JFrame jFrame = new JFrame();
+        JLabel usernameJLabel = new JLabel("Username");
+        jFrame.add(usernameJLabel);
+        final JTextField usernameTxtField = new JTextField();
+        usernameTxtField.setAlignmentX(JComponent.LEFT_ALIGNMENT);
+        jFrame.add(usernameTxtField);
+        JLabel passwordLabel = new JLabel("Password");
+        jFrame.add(passwordLabel);
+        final JPasswordField passwordField = new JPasswordField();
+        passwordField.setAlignmentX(JComponent.LEFT_ALIGNMENT);
+        jFrame.add(passwordField);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+        JPanel buttonsPanel = new JPanel();
+        JButton confirmBtn = new JButton("Đăng nhập");
+        confirmBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameTxtField.getText();
+                if (username.equals("giaovu")) {
+                    String password = String.valueOf(passwordField.getPassword());
+                    if (password.equals("giaovu")) {
+                        GiaoVu gv = new GiaoVu();
+                        gv.setConnection(DatabaseUtils.getDBConnection(GV_DB_USERNAME, GV_DB_PASSWORD));
+                        gv.showMainMenu();
+                        jFrame.dispose();
+                    }
+                } else {
+                    try {
+                        SinhVien sv = new SinhVien();
+                        sv.setConnection(DatabaseUtils.getDBConnection(SV_DB_USERNAME, SV_DB_PASSWORD));
+                        PreparedStatement statement = sv.getConnection().prepareStatement(
+                                "SELECT password FROM sinh_vien WHERE mssv = " + usernameTxtField.getText(),
+                                ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                        ResultSet rs = statement.executeQuery();
+                        if (!rs.next())
+                            JOptionPane.showMessageDialog(jFrame, "Username doesnt exist!", "Error",
+                                    JOptionPane.ERROR_MESSAGE);
+                        rs.beforeFirst();
+                        while (rs.next()) {
+                            String hashedPassword = rs.getString(1);
+                            if (hashedPassword
+                                    .equals(EncryptionUtils
+                                            .sha1FromString(String.valueOf(passwordField.getPassword())))) {
+                                sv.setMssv(Integer.parseInt(usernameTxtField.getText()));
+                                statement = sv.getConnection().prepareStatement(
+                                        "SELECT forceChangePass FROM sinh_vien WHERE mssv = " + username);
+                                ResultSet rs1 = statement.executeQuery();
+                                rs1.next();
+                                if (rs1.getInt(1) == 1) {
+                                    sv.changePassFirstLogin();
+                                } else
+                                    sv.showMainMenu();
+                                jFrame.dispose();
+                                return;
+                            }
+                        }
+                    } catch (SQLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                JOptionPane.showMessageDialog(jFrame, "Wrong username, password!", "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+        });
+        buttonsPanel.add(confirmBtn);
+        JButton cancelBtn = new JButton("Thoát");
+        cancelBtn.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(1);
+            }
+
+        });
+        buttonsPanel.add(cancelBtn);
+        buttonsPanel.setAlignmentX(JComponent.LEFT_ALIGNMENT);
+        buttonsPanel.setLayout(new FlowLayout());
+        jFrame.add(buttonsPanel);
+
+        jFrame.setTitle("Sinh viên đăng nhập");
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setVisible(true);
     }
 }
--- src/main/java/com/example/SinhVien.java
+++ src/main/java/com/example/SinhVien.java
@@ -313,7 +313,7 @@ public class SinhVien implements java.io.Serializable {
                 cancelBtn.addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        System.exit(1);
+                        jFrame.dispose();
                     }
                 });
                 buttonsPanel.add(cancelBtn);
@@ -399,83 +399,6 @@ public class SinhVien implements java.io.Serializable {
 
         jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
         jFrame.setTitle("Đổi mật khẩu");
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.pack();
-        jFrame.setVisible(true);
-    }
-
-    public void showLogin() {
-        final JFrame jFrame = new JFrame();
-        JLabel usernameJLabel = new JLabel("Username");
-        jFrame.add(usernameJLabel);
-        final JTextField usernameTxtField = new JTextField();
-        usernameTxtField.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-        jFrame.add(usernameTxtField);
-        JLabel passwordLabel = new JLabel("Password");
-        jFrame.add(passwordLabel);
-        final JPasswordField passwordField = new JPasswordField();
-        passwordField.setEchoChar('*');
-        passwordField.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-        jFrame.add(passwordField);
-        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
-        JPanel buttonsPanel = new JPanel();
-        JButton confirmBtn = new JButton("Đăng nhập");
-        confirmBtn.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    PreparedStatement statement = connection.prepareStatement(
-                            "SELECT password FROM sinh_vien WHERE mssv = " + usernameTxtField.getText(),
-                            ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                    ResultSet rs = statement.executeQuery();
-                    if (!rs.next())
-                        JOptionPane.showMessageDialog(jFrame, "Username doesnt exist!", "Error",
-                                JOptionPane.ERROR_MESSAGE);
-                    rs.beforeFirst();
-                    while (rs.next()) {
-                        String hashedPassword = rs.getString(1);
-                        if (hashedPassword
-                                .equals(EncryptionUtils.sha1FromString(String.valueOf(passwordField.getPassword())))) {
-                            setMssv(Integer.parseInt(usernameTxtField.getText()));
-                            statement = connection
-                                    .prepareStatement("SELECT forceChangePass FROM sinh_vien WHERE mssv = " + mssv);
-                            ResultSet rs1 = statement.executeQuery();
-                            rs1.next();
-                            if (rs1.getInt(1) == 1) {
-                                changePassFirstLogin();
-                            } else
-                                showMainMenu();
-                            jFrame.dispose();
-                        } else
-                            JOptionPane.showMessageDialog(jFrame, "Wrong username, password!", "Error",
-                                    JOptionPane.ERROR_MESSAGE);
-                    }
-                } catch (SQLException e1) {
-                    JOptionPane.showMessageDialog(jFrame, "Invalid username or password!", "Error",
-                            JOptionPane.ERROR_MESSAGE);
-                    e1.printStackTrace();
-                }
-            }
-
-        });
-        buttonsPanel.add(confirmBtn);
-        JButton cancelBtn = new JButton("Thoát");
-        cancelBtn.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(1);
-            }
-
-        });
-        buttonsPanel.add(cancelBtn);
-        buttonsPanel.setAlignmentX(JComponent.LEFT_ALIGNMENT);
-        buttonsPanel.setLayout(new FlowLayout());
-        jFrame.add(buttonsPanel);
-
-        jFrame.setTitle("Sinh viên đăng nhập");
-        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrame.pack();
         jFrame.setVisible(true);
